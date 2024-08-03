eb3be55a42e884632b096492d1c5536c88940ded
BudzioT <Budzio.zsm1@gmail.com>
2024-08-03 19:08:14 +0200

    Made animations work partially

--- Scenes/Entities/Player/player.gd
+++ Scenes/Entities/Player/player.gd
@@ -9,6 +9,8 @@ var direction: Vector2
 # Current road index that player's on
 var road_index: int = 0
 
+# Transformation variables
+@export_category("Transformations")
 # Player's height
 @export var height: int = 50
 
@@ -17,19 +19,28 @@ var vertical_movement: bool = false
 # Direction of vertical movement
 var vertical_direction: int = 0
 
+# Animations related variables
+@export_category("Animations")
 # Idle state flag
-var idle = false
+@export var state: String = "Run"
 
 
 """---------------------------- BUILT-IN FUNCTIONS ----------------------------"""
+func _ready():
+	"""Prepare the player"""
+	state = "Run"
+
 func _physics_process(_delta: float) -> void:
 	"""Process player's changes"""
-	if not idle:
+	if state != "Idle":
 		# Handle input
 		_handle_input()
 	
 		# Move the player
 		_move()
+		
+		# Animate him
+		_animate()
 
 """---------------------------- USER DEFINED FUNCTIONS ----------------------------"""
 func _handle_input():
@@ -55,18 +66,27 @@ func _handle_input():
 	
 	# If player's moving vertically, keep him accelerated
 	if vertical_movement:
-		# Create a tween for moving vertically
+		# Set the correct state
+		state = "Jump"
+		
+		# Calculate proper player's position on the new track
 		var newPos = Global.roads[road_index].y - int(float(height) / 2)
+		
+		# Create a tween for moving vertically
 		var tween = create_tween()
-		tween.tween_property(self, "position:y", newPos, Global.scroll_speed)
+		tween.tween_property(self, "position:y", newPos, 1.0 / Global.scroll_speed)
+		# On ending moving, return the animation to run
+		tween.tween_callback(_run)
+		
+		# Vertical movement is finished, turn off the flag
 		vertical_movement = false
 		
 	# Stop vertical movement
 	direction.y = 0
 	
 	# Check for attacks
-	if Input.is_action_just_pressed("Attack"):
-		_attack()
+	if Input.is_action_just_pressed("Attack") and not $Timers/AttackCooldown.time_left:
+		_explode()
 	
 		
 func _move():
@@ -77,8 +97,27 @@ func _move():
 	# Apply movement
 	move_and_slide()
 	
-func _attack():
+func _explode():
 	"""Make the player attack"""
-	pass
 	# Play the attack animation
+	state += "_Attack"
+	$AnimationPlayer.play(state)
+	
 	# Activate attack cooldown
+	$Timers/AttackCooldown.start()
+	
+func _animate():
+	"""Animate the player"""
+	# Play the run animation
+	if state == "Run":
+		$AnimationPlayer.play("Run")
+	# Play idle one
+	elif state == "Idle":
+		$AnimationPlayer.play("Idle")
+	# Jump animation
+	elif state == "Jump":
+		$AnimationPlayer.play("Jump")
+		
+func _run():
+	"""Make the player run again"""
+	state = "Run"
